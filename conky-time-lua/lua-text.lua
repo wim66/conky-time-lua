cd149a300971feecd498dbddbdee746c598837e2
Willem <20155866+wim66@users.noreply.github.com>
2024-06-13 10:25:31 +0200

    New version

--- conky-time-lua/lua-text.lua
+++ conky-time-lua/lua-text.lua
@@ -1,12 +1,10 @@
 --[[
 #########################################################
-# conky-time-lua					#
-# by +WillemO @wim66					#
-# 28-dec-16						#
-#							#
+# conky-time-lua					                    #
+# by +WillemO @wim66				                 	#
+# 28-dec-16					                         	#
+# v2 12-June-2024  (removed radial gradient)			#
 #########################################################
-
-
 ]]
 
 --[[TEXT WIDGET v1.3 by Wlourf 25/06/2010
@@ -69,266 +67,231 @@ v1.1	10/06/2010	Add "orientation" parameter
 v1.2	15/06/2010  Add "h_align", "v_align" and "radial" parameters
 v1.3	25/06/2010  Add "reflection_alpha", "reflection_length", "reflection_scale", 
                     "skew_x" et "skew_y"
+wim66   12/06/2024 Removed radial gradient, added optins for linear gradient
 
 
 ]]
+
 require 'cairo'
 
 function conky_draw_text()
-	--BEGIN OF PARAMETRES
-    text_settings={
+    --BEGIN OF PARAMETERS
+    text_settings = {
+        {
+            text = "9          2",
+            font_name = "Christmas Time Personal Use",
+            font_size = 90,
+            h_align = "c",
+            bold = false,
+            x = 280,
+            y = 70,
+            colour = {{0, 0xFFFFFF, 1}},
+        },
 
-		{
-			text="9          2",
-			font_name="Christmas Time Personal Use",
-			font_size=90,
-			h_align="c",
-			bold=false,
-			x=280,
-			y=70,
-			color={{0,0xFFFFFF,1}},
-		},
+        {
+            text = conky_parse("${time %A}"),
+            font_name = "Homestead inline",
+            font_size = 90,
+            h_align = "c",
+            bold = false,
+            x = 280,
+            y = 100,
+            orientation = "nn",
+            reflection_alpha = 0.5,
+            reflection_length = 0.5,
+            reflection_scale = 1.5,
+            colour = {{0., 0xFF0000, 1}, {0.5, 0xFFFFFF, 1}, {1, 0x0000FF, 1}},
+            linear_gradient = {150, 80, 150, 120},
+            v_align = "m",
+        },
 
-		{
-			text=conky_parse( "${time %A}" ),
-			font_name="Homestead inline",
-			font_size=90,
-			h_align="c",
-			bold=false,
-			x=280,
-			y=100,
-			orientation="nn",
-			reflection_alpha=0.5,
-			reflection_length=0.6,
-			reflection_scale=1.5,
-			colour={{0.98,0XFF0000,1},{0.99,0xFFFFFF,1}, {1,0x0000FF,1}},
-			v_align="m",
-			radial={0,-1000,0,0,-1000,1020}
+        {
+            text = conky_parse("${time %d %B}"),
+            font_name = "Homestead inline",
+            font_size = 32,
+            h_align = "c",
+            bold = false,
+            x = 280,
+            y = 205,
+            orientation = "nn",
+            colour = {{0, 0xFF0000, 1}, {0.5, 0xFFFFFF, 1}, {1, 0x0000FF, 1}},
+            linear_gradient = {150, 190, 150, 200},
+        },
 
-		},
+        {
+            text = conky_parse("${time %H:%M}"),
+            font_name = "Homestead inline",
+            font_size = 98,
+            h_align = "c",
+            bold = false,
+            x = 280,
+            y = 290,
+            orientation = "nn",
+            reflection_alpha = 0.5,
+            reflection_length = 0.5,
+            reflection_scale = 1.5,
+            colour = {{0, 0xFF0000, 1}, {0.5, 0xFFFFFF, 1}, {1, 0x0000FF, 1}},
+            linear_gradient = {150, 230, 150, 280},
+        },
+    }
 
-		{
-			text=conky_parse( "${time %d %B}" ),
-			font_name="Homestead inline",
-			font_size=32,
-			h_align="c",
-			bold=false,
-			x=280,
-			y=205,
-			orientation="nn",
-			colour={{0,0XFF0000,1},{0.5,0xFFFFFF,1}, {1,0x0000FF,1}},
-		},
+    --END OF PARAMETERS
 
-		{
-			text=conky_parse( "${time %H:%M}" ),
-			font_name="Homestead inline",
-			font_size=98,
-			h_align="c",
-			bold=false,
-			x=280,
-			y=290,
-			orientation="nn",
-			reflection_alpha=0.5,
-			reflection_length=0.6,
-			reflection_scale=1.5,
-			colour={{0,0XFF0000,1},{0.5,0xFFFFFF,1}, {1,0x0000FF,1}},
-		},
-	}
-
-
-    
---------------END OF PARAMETERES----------------
     if conky_window == nil then return end
-    if tonumber(conky_parse("$updates"))<3 then return end
-       
+    if tonumber(conky_parse("$updates")) < 3 then return end
+
     local cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual, conky_window.width, conky_window.height)
 
-    for i,v in pairs(text_settings) do    
-        cr = cairo_create (cs)
+    for i, v in pairs(text_settings) do    
+        cr = cairo_create(cs)
         display_text(v)
         cairo_destroy(cr)
     end
-    
+
     cairo_surface_destroy(cs)
-    
-
-
 end
 
 function rgb_to_r_g_b2(tcolour)
-    colour,alpha=tcolour[2],tcolour[3]
+    local colour, alpha = tcolour[2], tcolour[3]
     return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
 end
 
 function display_text(t)
-
-    local function set_pattern()
-        --this function set the pattern
-        if #t.colour==1 then 
-            cairo_set_source_rgba(cr,rgb_to_r_g_b2(t.colour[1]))
+    local function set_pattern(te)
+        -- this function sets the pattern
+        if #t.colour == 1 then 
+            cairo_set_source_rgba(cr, rgb_to_r_g_b2(t.colour[1]))
         else
-            local pat
-            
-            if t.radial==nil then
-                local pts=linear_orientation(t,te)
-                pat = cairo_pattern_create_linear (pts[1],pts[2],pts[3],pts[4])
-            else
-                pat = cairo_pattern_create_radial (t.radial[1],t.radial[2],t.radial[3],t.radial[4],t.radial[5],t.radial[6])
+            local pts = t.linear_gradient or linear_orientation(t, te)
+            local pat = cairo_pattern_create_linear(pts[1], pts[2], pts[3], pts[4])
+            for i = 1, #t.colour do
+                cairo_pattern_add_color_stop_rgba(pat, t.colour[i][1], rgb_to_r_g_b2(t.colour[i]))
             end
-        
-            for i=1, #t.colour do
-                cairo_pattern_add_color_stop_rgba (pat, t.colour[i][1], rgb_to_r_g_b2(t.colour[i]))
-            end
-            cairo_set_source (cr, pat)
+            cairo_set_source(cr, pat)
         end
     end
-    
-    --set default values if needed
-    if t.text==nil then t.text="Conky is good for you !" end
-    if t.x==nil then t.x = conky_window.width/2 end
-    if t.y==nil then t.y = conky_window.height/2 end
-    if t.colour==nil then t.colour={{1,0xFFFFFF,1}} end
-    if t.font_name==nil then t.font_name="Free Sans" end
-    if t.font_size==nil then t.font_size=14 end
-    if t.angle==nil then t.angle=0 end
-    if t.italic==nil then t.italic=false end
-    if t.oblique==nil then t.oblique=false end
-    if t.bold==nil then t.bold=false end
-    if t.radial ~= nil then
-        if #t.radial~=6 then 
-            print ("error in radial table")
-            t.radial=nil 
-        end
-    end
-    if t.orientation==nil then t.orientation="ww" end
-    if t.h_align==nil then t.h_align="l" end
-    if t.v_align==nil then t.v_align="b" end    
-    if t.reflection_alpha == nil then t.reflection_alpha=0 end
-    if t.reflection_length == nil then t.reflection_length=1 end
-    if t.reflection_scale == nil then t.reflection_scale=1 end
-    if t.skew_x==nil then t.skew_x=0 end
-    if t.skew_y==nil then t.skew_y=0 end    
-    cairo_translate(cr,t.x,t.y)
-    cairo_rotate(cr,t.angle*math.pi/180)
-    cairo_save(cr)       
-     
- 
+
+    -- set default values if needed
+    if t.text == nil then t.text = "Conky is good for you!" end
+    if t.x == nil then t.x = conky_window.width / 2 end
+    if t.y == nil then t.y = conky_window.height / 2 end
+    if t.colour == nil then t.colour = {{1, 0xFFFFFF, 1}} end
+    if t.font_name == nil then t.font_name = "Free Sans" end
+    if t.font_size == nil then t.font_size = 14 end
+    if t.angle == nil then t.angle = 0 end
+    if t.italic == nil then t.italic = false end
+    if t.oblique == nil then t.oblique = false end
+    if t.bold == nil then t.bold = false end
+    if t.orientation == nil then t.orientation = "ww" end
+    if t.h_align == nil then t.h_align = "l" end
+    if t.v_align == nil then t.v_align = "b" end    
+    if t.reflection_alpha == nil then t.reflection_alpha = 0 end
+    if t.reflection_length == nil then t.reflection_length = 1 end
+    if t.reflection_scale == nil then t.reflection_scale = 1 end
+    if t.skew_x == nil then t.skew_x = 0 end
+    if t.skew_y == nil then t.skew_y = 0 end    
+    cairo_translate(cr, t.x, t.y)
+    cairo_rotate(cr, t.angle * math.pi / 180)
+    cairo_save(cr)
 
     local slant = CAIRO_FONT_SLANT_NORMAL
-    local weight =CAIRO_FONT_WEIGHT_NORMAL
+    local weight = CAIRO_FONT_WEIGHT_NORMAL
     if t.italic then slant = CAIRO_FONT_SLANT_ITALIC end
     if t.oblique then slant = CAIRO_FONT_SLANT_OBLIQUE end
     if t.bold then weight = CAIRO_FONT_WEIGHT_BOLD end
-    
-    cairo_select_font_face(cr, t.font_name, slant,weight)
- 
-    for i=1, #t.colour do    
-        if #t.colour[i]~=3 then 
-            print ("error in color table")
-            t.colour[i]={1,0xFFFFFF,1} 
+
+    cairo_select_font_face(cr, t.font_name, slant, weight)
+
+    for i = 1, #t.colour do    
+        if #t.colour[i] ~= 3 then 
+            print("error in colour table")
+            t.colour[i] = {1, 0xFFFFFF, 1} 
         end
     end
 
     local matrix0 = cairo_matrix_t:create()
-    skew_x,skew_y=t.skew_x/t.font_size,t.skew_y/t.font_size
-    cairo_matrix_init (matrix0, 1,skew_y,skew_x,1,0,0)
-    cairo_transform(cr,matrix0)
-    cairo_set_font_size(cr,t.font_size)
-    te=cairo_text_extents_t:create()
-    cairo_text_extents (cr,t.text,te)
-    
-    set_pattern()
+    local skew_x, skew_y = t.skew_x / t.font_size, t.skew_y / t.font_size
+    cairo_matrix_init(matrix0, 1, skew_y, skew_x, 1, 0, 0)
+    cairo_transform(cr, matrix0)
+    cairo_set_font_size(cr, t.font_size)
+    local te = cairo_text_extents_t:create()
+    cairo_text_extents(cr, t.text, te)
 
+    set_pattern(te)
 
-            
-    mx,my=0,0
-    
-    if t.h_align=="c" then
-        mx=-te.width/2
-    elseif t.h_align=="r" then
-        mx=-te.width
+    local mx, my = 0, 0
+    if t.h_align == "c" then
+        mx = -te.width / 2
+    elseif t.h_align == "r" then
+        mx = -te.width
     end
-    if t.v_align=="m" then
-        my=-te.height/2-te.y_bearing
-    elseif t.v_align=="t" then
-        my=-te.y_bearing
+    if t.v_align == "m" then
+        my = -te.height / 2 - te.y_bearing
+    elseif t.v_align == "t" then
+        my = -te.y_bearing
     end
-    cairo_move_to(cr,mx,my)
-    
-    cairo_show_text(cr,t.text)
+    cairo_move_to(cr, mx, my)
 
-     
-        
-        
-   if t.reflection_alpha ~= 0 then 
+    cairo_show_text(cr, t.text)
+
+    if t.reflection_alpha ~= 0 then 
         local matrix1 = cairo_matrix_t:create()
-        cairo_set_font_size(cr,t.font_size)
+        cairo_set_font_size(cr, t.font_size)
+        cairo_matrix_init(matrix1, 1, 0, 0, -1 * t.reflection_scale, 0, (te.height + te.y_bearing + my) * (1 + t.reflection_scale))
+        cairo_set_font_size(cr, t.font_size)
+        te = cairo_text_extents_t:create()
+        cairo_text_extents(cr, t.text, te)
+        cairo_transform(cr, matrix1)
+        set_pattern(te)
+        cairo_move_to(cr, mx, my)
+        cairo_show_text(cr, t.text)
 
-        cairo_matrix_init (matrix1,1,0,0,-1*t.reflection_scale,0,(te.height+te.y_bearing+my)*(1+t.reflection_scale))
-        cairo_set_font_size(cr,t.font_size)
-        te=cairo_text_extents_t:create()
-        cairo_text_extents (cr,t.text,te)
-        
-                
-        cairo_transform(cr,matrix1)
-        set_pattern()
-        cairo_move_to(cr,mx,my)
-        cairo_show_text(cr,t.text)
-
-        local pat2 = cairo_pattern_create_linear (0,
-                                        (te.y_bearing+te.height+my),
-                                        0,
-                                        te.y_bearing+my)
-        cairo_pattern_add_color_stop_rgba (pat2, 0,1,0,0,1-t.reflection_alpha)
-        cairo_pattern_add_color_stop_rgba (pat2, t.reflection_length,0,0,0,1)    
-        
-        
-        cairo_set_line_width(cr,1)
-        dy=te.x_bearing
-        if dy<0 then dy=dy*(-1) end
-        cairo_rectangle(cr,mx+te.x_bearing,te.y_bearing+te.height+my,te.width+dy,-te.height*1.05)
+        local pat2 = cairo_pattern_create_linear(0, (te.y_bearing + te.height + my), 0, te.y_bearing + my)
+        cairo_pattern_add_color_stop_rgba(pat2, 0, 1, 0, 0, 1 - t.reflection_alpha)
+        cairo_pattern_add_color_stop_rgba(pat2, t.reflection_length, 0, 0, 0, 1)
+        cairo_set_line_width(cr, 1)
+        local dy = te.x_bearing
+        if dy < 0 then dy = dy * (-1) end
+        cairo_rectangle(cr, mx + te.x_bearing, te.y_bearing + te.height + my, te.width + dy, -te.height * 1.05)
         cairo_clip_preserve(cr)
-        cairo_set_operator(cr,CAIRO_OPERATOR_CLEAR)
-        --cairo_stroke(cr)
-        cairo_mask(cr,pat2)
+        cairo_set_operator(cr, CAIRO_OPERATOR_CLEAR)
+        cairo_mask(cr, pat2)
         cairo_pattern_destroy(pat2)
-        cairo_set_operator(cr,CAIRO_OPERATOR_OVER)
+        cairo_set_operator(cr, CAIRO_OPERATOR_OVER)
     end
-    
 end
 
+function linear_orientation(t, te)
+    local w, h = te.width, te.height
+    local xb, yb = 0, 0
+    if t.h_align == "c" then
+        xb = xb - w / 2
+    elseif t.h_align == "r" then
+        xb = xb - w
+    end    
+    if t.v_align == "m" then
+        yb = -h / 2
+    elseif t.v_align == "t" then
+        yb = 0
+    end
 
-function linear_orientation(t,te)
-    local w,h=te.width,te.height
-    local xb,yb=te.x_bearing,te.y_bearing
-    
-    if t.h_align=="c" then
-        xb=xb-w/2
-    elseif t.h_align=="r" then
-        xb=xb-w
-       end    
-    if t.v_align=="m" then
-        yb=-h/2
-    elseif t.v_align=="t" then
-        yb=0
-       end    
-       
-    if t.orientation=="nn" then
-        p={xb+w/2,yb,xb+w/2,yb+h}
-    elseif t.orientation=="ne" then
-        p={xb+w,yb,xb,yb+h}
-    elseif t.orientation=="ww" then
-        p={xb,h/2,xb+w,h/2}
-    elseif vorientation=="se" then
-        p={xb+w,yb+h,xb,yb}
-    elseif t.orientation=="ss" then
-        p={xb+w/2,yb+h,xb+w/2,yb}
-    elseif vorientation=="ee" then
-        p={xb+w,h/2,xb,h/2}        
-    elseif t.orientation=="sw" then
-        p={xb,yb+h,xb+w,yb}
-    elseif t.orientation=="nw" then
-        p={xb,yb,xb+w,yb+h}
+    local p
+    if t.orientation == "nn" then
+        p = {xb + w / 2, yb, xb + w / 2, yb + h}
+    elseif t.orientation == "ne" then
+        p = {xb + w, yb, xb, yb + h}
+    elseif t.orientation == "ww" then
+        p = {xb, h / 2, xb + w, h / 2}
+    elseif t.orientation == "se" then
+        p = {xb + w, yb + h, xb, yb}
+    elseif t.orientation == "ss" then
+        p = {xb + w / 2, yb + h, xb + w / 2, yb}
+    elseif t.orientation == "ee" then
+        p = {xb + w, h / 2, xb, h / 2}        
+    elseif t.orientation == "sw" then
+        p = {xb, yb + h, xb + w, yb}
+    elseif t.orientation == "nw" then
+        p = {xb, yb, xb + w, yb + h}
     end
     return p
 end
--- conky-time-lua/start.sh
+++ conky-time-lua/start.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-conky -c conky.conf
+#!/bin/bash
+
+# Bepaal het pad naar de Conky-map
+CONKY_DIR=$(dirname "$(readlink -f "$0")")
+
+# Controleer of Conky al draait en stop het
+if pidof conky > /dev/null; then
+    killall conky
+fi
+
+# Start Conky met de juiste configuratie en log fouten
+cd $CONKY_DIR
+conky -c ./conky.conf
